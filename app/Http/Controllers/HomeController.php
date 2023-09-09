66fb720f14e03c73045e423b013b7a4cdae32d4a
Doni Darmawan <doni.duaasattuu@gmail.com>
2023-09-10 00:37:45 +0700

    membuat message handler to telegram bot

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,6 +9,10 @@ class HomeController extends Controller
 {
     public function home(Request $request)
     {
-        return response()->view("home", ["title" => "Doni Darmawan"])->withHeaders(["Author" => "Doni Darmawan"]);
+        return response()->view(
+            "home",
+            ["title" => "Doni Darmawan"]
+        )
+            ->withHeaders(["Author" => "Doni Darmawan"]);
     }
 }
--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -3,21 +3,48 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Telegram\Bot\Laravel\Facades\Telegram;
 
 class MessageController extends Controller
 {
-    public function form(Request $request)
+    public function form(Request $request): Response
     {
         $name = $request->input("name");
         $email = $request->input("email");
         $message = $request->input("message");
 
-        return response()
-            ->json([
-                "name" => $name,
-                "email" => $email,
-                "message" => $message,
-            ]);
+        // return response()
+        //     ->json([
+        //         "name" => $name,
+        //         "email" => $email,
+        //         "message" => $message,
+        //     ]);
+
+        // $message = [
+        //     'name' => $name,
+        //     'email' => $email,
+        //     'message' => $message,
+        // ];
+
+        $messageResult = "Name : $name" . "\n" . "Email : $email" . "\n" . "Message : $message";
+
+        $response = Telegram::sendMessage([
+            'chat_id' => '@donidarmawanportfolioweb',
+            'text' => $messageResult
+        ]);
+
+        $messageId = $response->getMessageId();
+
+        return response()->view(
+            "home",
+            [
+                "title" => "Doni Darmawan",
+                "success" => true
+            ]
+        )
+            ->withHeaders(["Author" => "Doni Darmawan"]);
     }
 }
